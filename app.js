089dd8e016c97a0d76060c01d8f9d187eab37643
Alupheo Msumba <juniormsumba@gmail.com>
2021-02-23 12:41:46 +0100

    added a review model and added delete option

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { campgroundSchema ,reviewSchema} = require('./schemas.js');
 const mongoose = require('mongoose');
 const catchAsync = require('./Utilities/catchAsync');
 const ExpressError = require('./Utilities/ExpressError');
 const joi = require('joi');
+const Review = require('./models/review');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false})
 .then(() => {
     console.log("DATABASE CONNECTED")
 })
@@ -26,6 +29,28 @@ app.set('views',path.join(__dirname,'views'))
 app.engine('ejs',ejsMate)
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+
+
 
 //default route
 app.get('/', (req,res) =>{
@@ -59,7 +84,7 @@ app.get('/campgrounds/new', (req,res) =>{
 
 //view campground details 
 app.get('/campgrounds/:id',catchAsync(async (req,res) =>{
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/details',{campground})
 }));
 
@@ -83,8 +108,25 @@ app.delete('/campgrounds/:id',catchAsync(async(req,res) =>{
     res.redirect('/campgrounds');
 
 }));
+
+app.post('/campgrounds/:id/reviews',validateReview,  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 app.all('*',(req,res,next)=>{
-    next(new ExpressError('Page Not FOund',404))
+    next(new ExpressError('Page Not Found',404))
 })
 app.use((err,req,res,next)=>{
     const{statusCode = 500,message = 'Something Went wrong!'} = err;
--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const reviews = require('./review');
 const Schema = mongoose.Schema
 
 //new schema has been created 
@@ -7,7 +8,22 @@ const CampgroundSchema = new Schema({
     image:String,
     price:Number,
     description:String,
-    location:String
+    location:String,
+    reviews:[
+        {
+           type:Schema.Types.ObjectId,ref:'Review'
+        }
+    ]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 //export the database 
